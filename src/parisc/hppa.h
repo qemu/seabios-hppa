c85c3470fc4d21d6163c419d8aa0cb467414c937
Helge Deller <deller@gmx.de>
2026-03-13 18:32:54 +0100

    parisc: Use FIRMWARE_HIGH constant

--- src/parisc/hppa.h
+++ src/parisc/hppa.h
@@ -214,8 +214,8 @@ static inline u32 ror(u32 word, unsigned int shift)
 
 /* F_EXTEND() is used to access I/O ports. ROM_EXTEND() to access firmware ROM/RAM */
 #if defined(__LP64__)
-#define F_EXTEND(x)   ((unsigned long)(0xfffffffful<<32) | (x))
-#define ROM_EXTEND(x) ((unsigned long)(0xfffffff0ul<<32) | (x))
+#define F_EXTEND(x)   ((((unsigned long)0xfffffffful )<<32) | (x))
+#define ROM_EXTEND(x) ((((unsigned long)FIRMWARE_HIGH)<<32) | (x))
 #else
 #define F_EXTEND(x)   ((unsigned long)(x))
 #define ROM_EXTEND(x) ((unsigned long)(x))
