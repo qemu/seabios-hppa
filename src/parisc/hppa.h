be4e01b29dfea726e66f9d744c2d876e7e7e05e1
Helge Deller <deller@gmx.de>
2026-03-30 21:11:58 +0200

    parisc: SEABIOS_HPPA_VERSION 24
    New SeaBIOS-hppa release with various fixes for qemu-v11:
    - Initialize Astro chip with relevant LMMIO, GMMIO and others
    - Disable Artist on 64-bit machines
    - Fully implement parisc memory ranges
    - Change inventory to list CPU first for HP-UX
    - Allow 715 to boot from HP-UX 11iv1 CD-ROM
    - Prepare for MULTICELL machines
    - Initialize PCI_CACHE_LINE_SIZE PCI value
    - Call TOC/HPMC handler code from OS if installed
    
    Signed-off-by: Helge Deller <deller@gmx.de>

--- src/parisc/hppa.h
+++ src/parisc/hppa.h
@@ -3,8 +3,8 @@
 /* this file is included by x86.h */
 
 /* version number of this SeaBIOS firmware */
-#define SEABIOS_HPPA_VERSION 23
-#define SEABIOS_HPPA_VERSION_STR  "23"
+#define SEABIOS_HPPA_VERSION 24
+#define SEABIOS_HPPA_VERSION_STR  "24"
 
 #ifdef __LP64__
 #define CONFIG_64BIT
